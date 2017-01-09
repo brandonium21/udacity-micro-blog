d6abc4c0ec518dce66a5dca93c32d49a9132c690
Brandon Rice <brandonium21@gmail.com>
2017-01-09 15:53:24 -0600

    Added Comments and like functionality

--- app/Models.py
+++ app/Models.py
@@ -8,10 +8,18 @@ class Author(ndb.Model):
 	is_authenticated = ndb.BooleanProperty(indexed=True)
 	is_anonymous = ndb.BooleanProperty(indexed=True)
 
-
+class Comment(ndb.Model):
+	"""A sub model for representing an individual Comment."""
+	author = ndb.UserProperty(required = True)
+	content = ndb.StringProperty(indexed=True)
+	updated_on = ndb.DateTimeProperty(auto_now = True)
+	
 class Post(ndb.Model):
 	"""A main model for representing an individual Post."""
 	author = ndb.UserProperty(required = True)
 	content = ndb.StringProperty(indexed=True)
 	title = ndb.StringProperty(indexed=True)
-	updated_on = ndb.DateTimeProperty(auto_now_add = True)
+	updated_on = ndb.DateTimeProperty(auto_now = True)
+	comments = ndb.KeyProperty(kind='Comment', repeated=True)
+	likes = ndb.UserProperty(repeated= True)
+
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template, url_for, request, redirect
-from Models import Author, Post
+from Models import Author, Post, Comment
 from util import login_needed
 from werkzeug.security import generate_password_hash, check_password_hash
 from google.appengine.ext.db.metadata import Property
@@ -14,22 +14,28 @@ import datetime
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+def get_item(key=None):
+    return key.get()
+
 @app.route('/', methods=['GET','POST'])
 def show_allpost():
     """Return a friendly HTTP greeting."""
     logout = login = None
     user = users.get_current_user()
-    posts = Post.query()
+    posts = Post.query().fetch()
+    print posts
     if users.get_current_user():
         logout = users.create_logout_url(users.create_login_url(request.url))
     else:
         login = users.create_login_url(request.url)
 
     return render_template("allpost.html", 
-        posts=posts, user=user, 
+        posts=posts, user=user, get_item=get_item,
         logout=logout, login=login)
 
+
 @app.route('/editPost', methods=['GET','POST'])
+@login_needed
 def edit_post():
     """Return a friendly HTTP greeting."""
     if request.method == 'POST':
@@ -46,7 +52,9 @@ def edit_post():
             return redirect(url_for("show_allpost"))
         return render_template('unauthorized.html')
 
+
 @app.route('/deletePost/<post_key>', methods=['GET','POST'])
+@login_needed
 def del_post(post_key):
     """Return a friendly HTTP greeting."""
     del_post = ndb.Key(Post, int(post_key))
@@ -73,6 +81,78 @@ def show_addpost():
         time.sleep(.2)
         return redirect(url_for("show_allpost"))
     #return redirect(url_for("show_allpost", user=users.get_current_user()))
+
+
+@app.route('/addcomment', methods=['GET', 'POST'])
+@login_needed
+def show_addcomment():
+    """Return a friendly HTTP greeting."""
+    if request.method == 'POST':
+        post = ndb.Key(Post, int(request.form['post_id'])).get()
+        cmt = Comment(
+            content = request.form['content'],
+            author = users.get_current_user())
+        cmt.put()
+        post.comments.append(cmt.key)
+        post.put()
+        time.sleep(.2)
+        return redirect(url_for("show_allpost"))
+    #return redirect(url_for("show_allpost", user=users.get_current_user()))
+
+@app.route('/like/<post>', methods=['GET','POST'])
+@login_needed
+def like(post):
+    post = ndb.Key(Post, int(post)).get()
+    if post.author == users.get_current_user():
+        return render_template('unauthorized.html')
+    if users.get_current_user() in post.likes:
+        return redirect(url_for("show_allpost"))
+    post.likes.append(users.get_current_user())
+    post.put()
+    time.sleep(.2)
+    return redirect(url_for("show_allpost"))
+    
+
+@app.route('/unlike/<post_key>', methods=['GET','POST'])
+@login_needed
+def unlike(post_key):
+    post = ndb.Key(Post, int(post_key)).get()
+    if post.author == users.get_current_user():
+        return render_template('unauthorized.html')
+    if users.get_current_user() in post.likes:
+        indx = post.likes.index(users.get_current_user())
+        del post.likes[indx]
+        post.put()
+        time.sleep(.2)
+    return redirect(url_for("show_allpost"))
+        
+
+@app.route('/editComment', methods=['GET','POST'])
+@login_needed
+def edit_comment():
+    """Return a friendly HTTP greeting."""
+    if request.method == 'POST':
+        cmt_id = request.form['cmt_key']
+        content = request.form['content']
+        cmt = ndb.Key(Comment, int(cmt_id)).get()
+        if cmt.author == users.get_current_user():
+            cmt.content = content
+            cmt.put()
+            time.sleep(.2)
+            return redirect(url_for("show_allpost"))
+        return render_template('unauthorized.html')
+
+
+@app.route('/deleteCmt/<comment_key>', methods=['GET','POST'])
+@login_needed
+def del_comment(comment_key):
+    """Return a friendly HTTP greeting."""
+    del_comment = ndb.Key(Comment, int(comment_key))
+    if comment.author == users.get_current_user():
+        del_comment.delete()
+        time.sleep(.2)
+        return redirect(url_for("show_allpost"))
+    return render_template('unauthorized.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
